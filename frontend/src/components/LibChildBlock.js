9d2b3135bbf2c05587c182dd7c834a9d4c64dea2
Wertlexx <stud13.exe@gmail.com>
2025-06-18 22:39:18 +0300

    id:IDS-8 Added checkboxes on page and restructurized markup to fit the task

--- frontend/src/components/LibChildBlock.js
+++ frontend/src/components/LibChildBlock.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
+import Checkbox from '@mui/material/Checkbox';
+
+const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 
 const LibChildBlock = () => {
   return (
     <Container className="library-decorator">
       <Row>
         <Col xs={3} className="" />
-        <Col xs={9}>
-          <Row xs={12} className="library-body"></Row>
-          <Row className="footer">
-            <Col xs={8} className="library-btn-add"></Col>
-            <Col xs={4} className="library-btn-delete"></Col>
-          </Row>
+        <Col xs={7} className="library-body"></Col>
+        <Col xs={2} className="library-checkbox-container">
+          <Checkbox
+            {...label}
+            sx={{
+              color: '#000000',
+              '&.Mui-checked': {
+                color: '#000000',
+              },
+            }}
+          ></Checkbox>
         </Col>
+      </Row>
+      <Row className="footer">
+        <Col xs={3} />
+        <Col xs={6} className="library-btn-add"></Col>
+        <Col xs={3} className="library-btn-delete"></Col>
       </Row>
     </Container>
   );
